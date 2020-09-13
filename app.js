be2e53e566a112152d7d18dbcf38b04d7b12ce7c
Shouraya <shourayagoyal2406@gmail.com>
2020-09-13 13:38:10 +0530

    calculator added

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ app.post("/addfriend", function(req,res){
 	friends.push(newFriend);
 	res.redirect("/friends");
 })
+// made calculator extra... the below two are for that only
+app.get("/calculator", function(req,res){
+	res.render("calculator")
+})
+	
+app.post("/output", function(req,res){
+	var val1 = req.body.val1;
+	var val2 = req.body.val2;
+	res.render("output", {val1 : val1, val2 : val2});
+});	
+
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log("Server has Started!!");
